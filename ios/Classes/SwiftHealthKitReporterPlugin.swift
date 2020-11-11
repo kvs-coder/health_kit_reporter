d86ea2b24f5fcd53fb977ad7d5628f4f45cf108d
Victor Kachalov <victor.kachalov@thryve.de>
2020-11-11 18:48:33 +0100

    sample app launch, problem - uses old HKRepo

--- ios/Classes/SwiftHealthKitReporterPlugin.swift
+++ ios/Classes/SwiftHealthKitReporterPlugin.swift
@@ -1,7 +1,36 @@
 import Flutter
 import UIKit
+import HealthKitReporter
 
 public class SwiftHealthKitReporterPlugin: NSObject, FlutterPlugin {
+    private enum Method: String {
+        case characteristicsQuery
+        case quantityQuery
+        case categoryQuery
+        case workoutQuery
+        case electrocardiogramQuery
+        case sampleQuery
+        case statisticsQuery
+        case statisticsCollectionQuery
+        case heartbeatSeriesQuery
+        case queryActivitySummary
+        case anchoredObjectQuery
+        case sourceQuery
+        case correlationQuery
+        case observerQuery
+        case enableBackgroundDelivery
+        case disableAllBackgroundDelivery
+        case disableBackgroundDelivery
+        case requestAuthorization
+        case preferredUnits
+        case startWatchApp
+        case isAuthorizedToWrite
+        case addCategory
+        case addQuantitiy
+        case delete
+        case save
+    }
+
   public static func register(with registrar: FlutterPluginRegistrar) {
     let channel = FlutterMethodChannel(name: "health_kit_reporter", binaryMessenger: registrar.messenger())
     let instance = SwiftHealthKitReporterPlugin()
@@ -9,6 +38,81 @@ public class SwiftHealthKitReporterPlugin: NSObject, FlutterPlugin {
   }
 
   public func handle(_ call: FlutterMethodCall, result: @escaping FlutterResult) {
-    result("iOS " + UIDevice.current.systemVersion)
+    guard let method = Method.init(rawValue: call.method) else {
+        result(
+            FlutterError(
+                code: #function,
+                message: "Please check the method",
+                details: "Provied method: \(call.method)"
+            )
+        )
+        return
+    }
+    do {
+//        let reporter = try HealthKitReporter()
+//        switch method {
+//        case .characteristicsQuery:
+//            let characteristics = try reporter.reader.characteristicsQuery()
+//            let data = try JSONEncoder().encode(characteristics)
+//            let jsonString = String(data: data, encoding: .utf8)
+//            result(jsonString)
+//        case .quantityQuery:
+//            try reporter.reader
+//        case .categoryQuery:
+//            <#code#>
+//        case .workoutQuery:
+//            <#code#>
+//        case .electrocardiogramQuery:
+//            <#code#>
+//        case .sampleQuery:
+//            <#code#>
+//        case .statisticsQuery:
+//            <#code#>
+//        case .statisticsCollectionQuery:
+//            <#code#>
+//        case .heartbeatSeriesQuery:
+//            <#code#>
+//        case .queryActivitySummary:
+//            <#code#>
+//        case .anchoredObjectQuery:
+//            <#code#>
+//        case .sourceQuery:
+//            <#code#>
+//        case .correlationQuery:
+//            <#code#>
+//        case .observerQuery:
+//            <#code#>
+//        case .enableBackgroundDelivery:
+//            <#code#>
+//        case .disableAllBackgroundDelivery:
+//            <#code#>
+//        case .disableBackgroundDelivery:
+//            <#code#>
+//        case .requestAuthorization:
+//            <#code#>
+//        case .preferredUnits:
+//            <#code#>
+//        case .startWatchApp:
+//            <#code#>
+//        case .isAuthorizedToWrite:
+//            <#code#>
+//        case .addCategory:
+//            <#code#>
+//        case .addQuantitiy:
+//            <#code#>
+//        case .delete:
+//            <#code#>
+//        case .save:
+//            <#code#>
+//        }
+    } catch {
+        result(
+            FlutterError(
+                code: #function,
+                message: "Error initializing HealthKitReporter",
+                details: "\(error)"
+            )
+        )
+    }
   }
 }
--- ios/health_kit_reporter.podspec
+++ ios/health_kit_reporter.podspec
@@ -6,18 +6,20 @@ Pod::Spec.new do |s|
   s.name             = 'health_kit_reporter'
   s.version          = '1.1.1'
   s.summary          = 'A new Flutter project.'
+  s.swift_versions   = '5.3'
   s.description      = <<-DESC
-A new Flutter project.
+'Helps to write or read data from Apple Health via HealthKit framework.'
                        DESC
-  s.homepage         = 'http://example.com'
-  s.license          = { :file => '../LICENSE' }
-  s.author           = { 'Your Company' => 'email@example.com' }
+  s.homepage         = 'https://github.com/VictorKachalov/health_kit_reporter'
+  s.license          = { :type => 'MIT', :file => '../LICENSE' }
+  s.author           = { 'Victor Kachalov' => 'victorkachalov@gmail.com' }
   s.source           = { :path => '.' }
+  s.ios.deployment_target = '12.0'
   s.source_files = 'Classes/**/*'
   s.dependency 'Flutter'
-  s.platform = :ios, '8.0'
+  s.dependency 'HealthKitReporter'
+  s.platform = :ios, '12.0'
 
   # Flutter.framework does not contain a i386 slice.
   s.pod_target_xcconfig = { 'DEFINES_MODULE' => 'YES', 'EXCLUDED_ARCHS[sdk=iphonesimulator*]' => 'i386' }
-  s.swift_version = '5.0'
 end
